05902f387dea348d57161c626120cba3e2303da8
softweredevloper <ssayam200@gmail.com>
2023-06-10 03:17:56 +0530

    commit as work done

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,3 +257,41 @@
   new PureCounter();
 
 })()
+
+var form = document.getElementById("my-form");
+var submit = document.getElementById("submit-btn")
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          // show my existiong message which is hide by default
+          document.getElementById("loading").style.display = "block";
+          document.getElementById("sent-message").style.display = "block";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+            } else {
+              document.getElementById("loading").style.display = "block";
+              document.getElementById("error-message").style.display = "block";
+              document.getElementById("error-message").innerHTML = "Error!!!"   
+             form.reset()
+            }
+          })
+        }
+      }).catch(error => {
+        document.getElementById("loading").style.display = "block";
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").innerHTML = "Error!!!"
+        form.reset()
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
